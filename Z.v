e3b51c0a1beebdb3ad0146446f691aa82d647369
Daniel K Lyons <fusion@storytotell.org>
2019-12-10 23:08:02 -0700

    starting on some proof stuff

---
+++ Z.v
@@ -0,0 +1,66 @@
+From Coq Require Import ssreflect ssrfun ssrbool.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+(* This is a scratch pad for dealing with Chapter 2 of Using Z: Specification, Refinement, Proof *)
+Theorem and_comm : forall P Q : Prop, P /\ Q -> Q /\ P.
+Proof.
+  intros P Q [HP HQ].
+  split.
+  - apply HQ.
+  - apply HP.
+Qed.
+
+
+(* Here's an attempt to redo it using SSReflect *)
+Theorem and_comm' : forall P Q : Prop, P /\ Q -> Q /\ P.
+Proof.
+  move => P Q [HP HQ].
+  by split.
+Qed.
+
+
+Theorem disj_comm : forall P Q : Prop, P \/ Q -> Q \/ P.
+Proof.
+  intros P Q H.
+  destruct H. right. apply H.
+  left. apply H.
+Qed.
+
+(* And here's disj_comm in ssreflect *)
+Theorem disj_comm' : forall P Q : Prop, P \/ Q -> Q \/ P.
+Proof.
+  move => P Q H.
+  case H.
+  right.
+(* didn't quite figure this one out. probably need to understand more before getting to excited about ssreflect *)
+Abort.
+
+(* Here's another proof from Using Z *)
+Theorem paqir_implies_piqir : forall P Q R : Prop, (P /\ Q -> R) -> (P -> (Q -> R)).
+Proof.
+  intros P Q R H HP HQ.
+  assert (HPQ: P /\ Q).
+  split. exact HP. exact HQ.
+  pose (HR := H HPQ).
+  exact HR.
+  (* I worry this is on the ugly side for what I'm trying to do. *)
+Qed.
+
+(* These two were stolen from a tutorial *)
+
+Theorem backward_small : (forall A B : Prop,  A -> (A -> B) -> B).
+Proof.
+  intros A B.
+  intros proof_of_A A_implies_B.
+  refine (A_implies_B _).
+  exact proof_of_A.
+Qed.
+
+Theorem forward_small : forall A B : Prop, A -> (A -> B) -> B.
+Proof.
+  intros A B proof_of_A A_implies_B.
+  pose (proof_of_B := A_implies_B proof_of_A).
+  exact proof_of_B.
+Qed.
