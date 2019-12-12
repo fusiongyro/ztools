846607e05c775ca3abebf3ea329d2d223ec8cf2d
Daniel K Lyons <fusion@storytotell.org>
2019-12-11 22:14:08 -0700

    Incorporating commentary from Tyler

--- Z.v
+++ Z.v
@@ -6,6 +6,19 @@ Unset Printing Implicit Defensive.
 (* This is a scratch pad for dealing with Chapter 2 of Using Z: Specification, Refinement, Proof *)
 Theorem and_comm : forall P Q : Prop, P /\ Q -> Q /\ P.
 Proof.
+  (* TYLER: Ok, I'm doing this in CoqIDE, like a god-damn Caveman... 
+            I switched to vim a while ago, and I just really dont want
+            to jack with anything.
+
+            So before I critique (hmm, no spelling... just bare with me)
+            let's just write this out by hand:
+       Assuming I know P and Q, can I show Q and P?
+         to show Q and P, I must first show Q, then P
+         I know Q
+         I know P
+         QED.
+
+      What does this look like in Coq?*)
   intros P Q [HP HQ].
   split.
   - apply HQ.
@@ -16,10 +29,12 @@ Qed.
 (* Here's an attempt to redo it using SSReflect *)
 Theorem and_comm' : forall P Q : Prop, P /\ Q -> Q /\ P.
 Proof.
+  (* TYLER: Yeah, I've... literally never used ssreflect before, so
+            reading up really quick...
+  *)
   move => P Q [HP HQ].
   by split.
 Qed.
-
 
 Theorem disj_comm : forall P Q : Prop, P \/ Q -> Q \/ P.
 Proof.
@@ -29,7 +44,7 @@ Proof.
 Qed.
 
 (* and here's disj_comm in ssreflect *)
-Lemma disj_comm' : forall P Q : Prop, P \/ Q -> Q \/ P.
+Theorem disj_comm' : forall P Q : Prop, P \/ Q -> Q \/ P.
 Proof. move => P Q. by case; [right | left]. Qed.
 
 (* Here's another proof from Using Z *)
@@ -42,6 +57,15 @@ Proof.
   exact HR.
   (* I worry this is on the ugly side for what I'm trying to do. *)
 Qed.
+
+(* TYLER: Yeah, that was ugly. Let me try: *)
+Theorem paqir_implies_piqir' : forall P Q R : Prop, (P /\ Q -> R) -> (P -> (Q -> R)).
+Proof.
+  intros P Q R H HP HQ.
+  assert (HPQ: P /\ Q). split. exact HP. exact HQ.
+  apply H. exact HPQ.
+Qed.
+(* TYLER: So I have never used pose before. Seems fine. So bassically the same *)
 
 (* These two were stolen from a tutorial *)
 
